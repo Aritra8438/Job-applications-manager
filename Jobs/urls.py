86f12f0b138d2d3f0e16f07755257097dd35a49d
raisachatterjee <raisachatterjee2701@gmail.com>
2023-06-04 22:32:30 +0530

    Job Model Added

--- Jobs/urls.py
+++ Jobs/urls.py
@@ -1,6 +1,13 @@
 from .views import *
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router=DefaultRouter()
+router.register("Jobs", views.JobsViewSet) 
 
 urlpatterns = [
     path("", hello),
 ]
+
+urlpatterns+=router.urls
