f8c22c38511b8fbc24b57531e0c821ee3249eea7
raisachatterjee <raisachatterjee2701@gmail.com>
2023-06-06 18:03:47 +0530

    Authentication added, User model modified, Implemented acces and refresh tokens for register, login,logout. Register,login,logoutrefresh views added.

--- Jobs/urls.py
+++ Jobs/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import views
 
+
 router=DefaultRouter()
 router.register("", views.JobsViewSet) 
 
 urlpatterns = [
     path("hello/", hello),
+    
 ]
 
 urlpatterns+=router.urls
--- Users/models.py
+++ Users/models.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
+class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
+
+    first_name = None
+    last_name = None
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
