61c20e1cf45fd0fecbfea3ba26630ce279db4692
Aritra Majumder <aritramajumder8438@gmail.com>
2023-06-06 12:40:50 +0530

    Register model and update models.py

--- job_application/models.py
+++ job_application/models.py
@@ -2,18 +2,20 @@ from django.db import models
 from Jobs.models import Jobs
 from Users.models import User
 
-status = (
-    ('val1', 'Applied'),
-    ('val2', 'Under Consideration'),
-    ('val3', 'Interviewed'),
-    ('val4', 'declined')
-)
-# Create your models here.
+
 class job_applications(models.Model):
     id = models.IntegerField(primary_key=True)
     job_url = models.ForeignKey(Jobs, on_delete=models.CASCADE)
     time = models.TimeField(auto_now_add=True)
-    status = models.CharField(max_length=20,choices=status)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
 
+    class Status(models.TextChoices):
+        APPLIED = "Applied"
+        UNDER_CONSIDERATION = "Under Consideration"
+        INTERVIEW = "Interview"
+        DECLINED = "Declined"
+        ACCEPTED = "Accepted"
+
+    status = models.CharField(
+        max_length=30, choices=Status.choices, default=Status.APPLIED
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
