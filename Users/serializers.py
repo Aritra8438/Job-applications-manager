ccf12472b6b3b6aff96d34e23e4fb01930119de2
raisachatterjee <raisachatterjee2701@gmail.com>
2023-06-06 18:03:47 +0530

    Authentication added, User model modified, Implemented acces and refresh tokens for register, login,logout. Register,login,logoutrefresh views added.

--- Users/serializers.py
+++ Users/serializers.py
@@ -1,9 +1,20 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from .models import User
 
-
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'username', 'password']
-        
+        fields = ['id', 'name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            name=validated_data['name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user    
+
+    
