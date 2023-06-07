b8cd9a5e39095d34e8b457fc540d50e75b448d8b
raisachatterjee <raisachatterjee2701@gmail.com>
2023-06-07 15:31:41 +0530

    CV uploading feature implemented using CV model

--- Users/models.py
+++ Users/models.py
@@ -5,10 +5,33 @@ class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    
 
     first_name = None
     last_name = None
     username = None
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
+    
+class CV(models.Model):
+    CHOICES = [
+    ("Technical", "Technical"),
+    ("Marketing", "Marketing"),
+    ("Education", "Education"),
+    ("Govt_Job", "Govt_job"),
+    ]  
+
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+    job_type = models.CharField(max_length=100, choices=CHOICES)
+    resume=models.FileField(upload_to='documents/')
+    resume_name=models.CharField(max_length=50)
+    
+    @staticmethod
+    def get_all_CVs_by_user(id):
+
+        # Check if store ID was passed
+        if id:
+            return CV.objects.filter(user_id=id)
