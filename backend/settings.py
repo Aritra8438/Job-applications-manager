c3fe210089f28d5ae3179327f3cf17a4d425b5b6
Pulak <pulakkumarghosh2001@gmail.com>
2023-06-09 02:09:23 +0530

    Postgresql connected

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,11 +82,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Job',
+#         'USER': 'postgres',
+#         'PASSWORD': 'pulak',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse("postgres://root:1TnWTIdsRF7SA1oHNPkM5vlmXdpbnDcU@dpg-ci13gdd269v7c0vgd5o0-a.oregon-postgres.render.com/job_manager")
 }
 
 
